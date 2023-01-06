04d09ade1730103b1f77ffebf16af1e6942d8b59
Ben Sales <adebsa2401@gmail.com>
2023-01-07 00:16:01 +0100

    python import modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
