4265b5e840cd60dd850cedead2eaab177d3831be
Ben Sales <adebsa2401@gmail.com>
2023-01-12 01:20:30 +0100

    fix roman to int task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,46 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    Converts a Roman numeral to an integer.
-    ...
-
-    Parameters
-    ----------
-    roman_string : str
-        the roman numeral to convert
-
-    Return:
-        0 If the roman_string is not a string or None
-        The corresponding integer otherwise.
-    """
-
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
+    # Fail checks, none, not a string
+    if not roman_string:
         return 0
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        return 0
+    # Dictionary for roman numerals
+    r_dict = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
-    num = 0
 
-    for i in range(len(roman_string)):
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-        else:
-            num += roman_dict[roman_string[i]]
-
-    return num
+    result = 0
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx += 1
+    # Search in dict for correct numbers and add
+    for k, v in r_dict.items():
+        for index in temp:
+            if index == k:
+                result += v
+    return result
+    
