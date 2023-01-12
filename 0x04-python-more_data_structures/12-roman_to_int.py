fbb49ffc2e4cb12146f2281fa66200b5db7f8b2a
Ben Sales <adebsa2401@gmail.com>
2023-01-12 01:29:28 +0100

    fix roman to int task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,7 +14,7 @@ def roman_to_int(roman_string):
 
     if (not isinstance(roman_string, str) or
             roman_string is None):
-        return (0)
+        return 0
 
     roman_dict = {
             "I": 1,
@@ -27,15 +27,14 @@ def roman_to_int(roman_string):
     }
     num = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
+    for i, roman in enumerate(roman_string):
+        if roman_dict.get(roman) is None:
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+                roman_dict[roman] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman] * -1
         else:
-            num += roman_dict[roman_string[i]]
+            num += roman_dict[roman]
 
-    return (num)
-    
+    return num
